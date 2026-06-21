50ef1e5cff5f73bdcaa319eb58250048735b1e48
Pichu224 <alaninsaurralde2244@gmail.com>
2026-06-21 15:49:49 -0300

    feature: initializing git repository with proyect's structure

---
+++ src/main/java/com/vorynt/vorynt_api/VoryntApiApplication.java
@@ -0,0 +1,13 @@
+package com.vorynt.vorynt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoryntApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoryntApiApplication.class, args);
+	}
+
+}
